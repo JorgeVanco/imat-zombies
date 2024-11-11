c9d814df9023a009c66f45f0352bf2c1d1f5247c
Lydia Ruiz Martínez <lydia.ruiz.martz@gmail.com>
2024-11-11 20:05:18 +0100

    Añadido proyecto unity

---
+++ imat-zombies/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void EscenaJuego()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
