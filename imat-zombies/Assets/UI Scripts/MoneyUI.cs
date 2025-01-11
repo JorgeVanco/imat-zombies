3f08965763429efc9d23e5123da2621cc969b645
Lydia Ruiz Martínez <lydia.ruiz.martz@gmail.com>
2025-01-11 10:12:26 +0100

    Added Buying Menu

--- imat-zombies/Assets/UI Scripts/MoneyUI.cs
+++ imat-zombies/Assets/UI Scripts/MoneyUI.cs
@@ -2,12 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
+//public class MoneyUI : MonoBehaviour
+//{
+//    [SerializeField] private TextMeshProUGUI moneyText;
+
+//    public void UpdateMoneyText(int value)
+//    {
+//        moneyText.text = $"Money: {value}";
+
+//    }
+//}
+
 public class MoneyUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI moneyText;
+    private int currentMoney;
 
-    public void UpdateMoneyText(int value) {
-        moneyText.text = $"Money: {value}";
+    void Start()
+    {
+        InitializeMoney(100); 
+    }
 
+    public void InitializeMoney(int startingMoney)
+    {
+        currentMoney = startingMoney;
+        UpdateMoneyText(currentMoney);
+    }
+        
+    public void DecreaseMoney(int amount)
+    {
+        if (currentMoney >= amount)
+        {
+            currentMoney -= amount;
+            UpdateMoneyText(currentMoney);
+        }
+        else
+        {
+            Debug.Log("No tienes suficiente dinero");
+        }
+    }
+
+    public void UpdateMoneyText(int value)
+    {
+        currentMoney = value;
+        moneyText.text = $"Money: {currentMoney}";
     }
 }
+
